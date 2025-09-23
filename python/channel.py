89fb66a94f9bf729072b293734a49ffd376eda37
Kovacsics Robert <kovirobi@gmail.com>
2025-09-23 22:08:48 +0100

    Only warn once about an ignored channel

--- python/channel.py
+++ python/channel.py
@@ -12,7 +12,7 @@ class Channel(IntEnum):
 class Channels:
     def __init__(self, transport: Transport, loop=get_event_loop()) -> None:
         self._transport = transport
-        self._channels: dict[int, Future[bytes]] = {}
+        self._channels: dict[int, Future[bytes] | None] = {}
         self._loop = loop
 
     def open_channel(self, channel: int):
@@ -25,14 +25,21 @@ class Channels:
                 chan, data = await self._transport.recv()
                 if chan in self._channels:
                     fut = self._channels[chan]
+                    if fut is None:
+                        continue
                     if fut.done():
                         fut = self._loop.create_future()
                         fut.set_result(data)
                         self._channels[chan] = fut
                     else:
-                        self._channels[chan].set_result(data)
+                        fut.set_result(data)
                 else:
-                    print("Dropping data", data)
+                    name = chan
+                    if chan in Channel:
+                        name = Channel(chan).name
+                    print("Dropping channel", name, "data", data)
+                    print("future messages ignored on channel", name)
+                    self._channels[chan] = None
             except IncompleteReadError:
                 break
             except Exception as e:
@@ -44,6 +51,9 @@ class Channels:
         await self._transport.send(channel, data, timeout=timeout)
 
     async def recv(self, channel: int, *, timeout: float = DEFAULT_TIMEOUT) -> bytes:
-        result = await wait_for(shield(self._channels[channel]), timeout=timeout)
-        self._channels[channel] = self._loop.create_future()
+        fut = self._channels.get(channel)
+        if fut is None:
+            fut = self._loop.create_future()
+            self._channels[channel] = fut
+        result = await wait_for(shield(fut), timeout=timeout)
         return result
