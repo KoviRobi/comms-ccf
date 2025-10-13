1a03b4f452e375ea40b6aea4647090accc2faf41
Kovacsics Robert <kovirobi@gmail.com>
2025-10-13 10:35:10 +0100

    Add FreeRTOS-Demo/makefile and CI job
    This way we don't have to commit the misc files, and building is
    simple/doesn't require the sys.path hack.

--- python/binutils_mapfile/__main__.py
+++ python/binutils_mapfile/__main__.py
@@ -2,6 +2,7 @@
 Tool for analysing a mapfile, or comparing two mapfiles.
 """
 
+import re
 import sys
 from argparse import ArgumentParser
 from glob import glob
@@ -15,6 +16,14 @@ from svg import svg
 from yaml import diffable_format
 
 
+def extract_number(s: str) -> tuple[tuple[int, ...], str]:
+    """
+    Try to do human-numeric sort: Extract digits first, then the whole
+    string as a backup.
+    """
+    return (tuple(int(d) for d in re.findall(r"\d+", s)), s)
+
+
 def main():
     """
     Parse the given mapfile and format it in a way which is amenable
@@ -25,7 +34,11 @@ def main():
     parser.add_argument("--yaml", type=Path, help="Output a YAML suitable for diff")
     parser.add_argument("--svg", type=Path, help="Output an SVG treemap")
     args = parser.parse_args()
-    mapfiles = [Path(mapfile) for glob in args.mapfile for mapfile in glob]
+    mapfiles = [
+        Path(mapfile)
+        for glob in args.mapfile
+        for mapfile in sorted(glob, key=extract_number)
+    ]
 
     previous = None
     for n, mapfile in enumerate(mapfiles):
@@ -37,7 +50,8 @@ def main():
             if not args.yaml and not args.svg:
                 diffable_format(mapfile, sys.stdout)
             if args.yaml:
-                yaml_file = args.yaml
+                yaml_file: Path = args.yaml
+                yaml_file.parent.mkdir(exist_ok=True, parents=True)
                 if len(mapfiles) > 1:
                     yaml_file = args.yaml.with_suffix(f".{n}{args.yaml.suffix}")
                 with yaml_file.open("wt") as yaml_out:
@@ -45,9 +59,11 @@ def main():
                 if diff is not None:
                     yaml_file = args.yaml.with_suffix(f".{n-1}-{n}{args.yaml.suffix}")
                     with yaml_file.open("wt") as yaml_out:
+                        yaml_out.write(f"## Comparing {mapfiles[n-1]} to {mapfiles[n]}\n\n")
                         diff_md(diff, yaml_out)
             if args.svg:
-                svg_file = args.svg
+                svg_file: Path = args.svg
+                svg_file.parent.mkdir(exist_ok=True, parents=True)
                 if len(mapfiles) > 1:
                     svg_file = args.svg.with_suffix(f".{n}{args.svg.suffix}")
                 with svg_file.open("wt") as svg_out:
